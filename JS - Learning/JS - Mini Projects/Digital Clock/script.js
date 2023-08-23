668aab6fa2bf7e5fbebfc5bc5e724c57b9f2494f
Subha <dhonidinesh183@gmail.com>
2023-08-23 21:47:33 +0530

    Mini-Projects

---
+++ JS - Learning/JS - Mini Projects/Digital Clock/script.js
@@ -0,0 +1,24 @@
+function displayTime(){
+  var dateTime = new Date();
+  var hrs = dateTime.getHours();
+  var min = dateTime.getMinutes();
+  var sec = dateTime.getSeconds();
+  var sess = document.getElementById('session');
+ 
+  if(hrs >= 12){
+    sess.innerHTML = "PM";
+  }else{
+    sess.innerHTML = "AM";
+  }
+
+  if(hrs > 12){
+    hrs -= 12;
+  }
+
+  document.getElementById('hours').innerHTML = hrs;
+  document.getElementById('minutes').innerHTML = min;
+  document.getElementById('seconds').innerHTML = sec; 
+
+}
+
+setInterval(displayTime, 10);
