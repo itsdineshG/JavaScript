b4766984cf9e6308b5e1a7a3cefbaca4dfcc1318
Subha <dhonidinesh183@gmail.com>
2023-08-25 22:05:44 +0530

    done

--- JS - Learning/JS - Mini Projects/Color Flipper/script.js
+++ JS - Learning/JS - Mini Projects/Color Flipper/script.js
@@ -1,17 +1,32 @@
 const toggle = document.getElementById('toggleDark');
+const toggle1 = document.getElementById('toggleDark1');
 const body = document.querySelector('.bg-dark');
+const body1 = document.querySelector('.bg-dark1');
 
 
 toggle.addEventListener('click', function(){
-  this.classList.toggle('bi-moon-fill');
+  this.classList.toggle('bi-moon-stars-fill');
   if(this.classList.toggle('bi-brightness-high-fill')){
-    document.querySelector('.bg-dark').style.background = 'white';
-    document.querySelector('.bg-dark').style.color = '#000';
-    document.querySelector('.bg-dark').style.transition = '2s';
+    body.style.background = 'white';
+    body.style.color = '#000';
+    body.style.transition = '2s';
   }else{
-    querySelector('.bg-dark').style.background = 'black';
-    querySelector('.bg-dark').style.color = 'white';
-    querySelector('.bg-dark').style.transition = '2s';
+    body.style.background = 'black';
+    body.style.color = 'white';
+    body.style.transition = '2s';
+  }
+})
+
+toggle1.addEventListener('click', function(){
+  this.classList.toggle('bi-brightness-high-fill');
+  if(this.classList.toggle('bi-moon-stars-fill')){
+    body1.style.background = '#000';
+    body1.style.color = '#fff';
+    body1.style.transition = '2s';
+  }else{
+    body1.style.background = '#fff';
+    body1.style.color = '#000';
+    body1.style.transition = '2s';
   }
 })
 
